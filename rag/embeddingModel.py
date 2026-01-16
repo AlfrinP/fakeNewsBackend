fec36ce769a9a257dbf2ee62b36efe54e42b5d6d
alfrinP <alfrinpoulose@gmail.com>
2026-01-17 01:10:34 +0530

    feat: enhance environment variable management and update embedding model integration

--- rag/embeddingModel.py
+++ rag/embeddingModel.py
@@ -3,4 +3,4 @@ from textSplitters import texts
 
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 
-print(embeddings.embed_documents(texts))
+exported_embeddings = embeddings.embed_documents(texts)
--- rag/vectorDb.py
+++ rag/vectorDb.py
@@ -1,10 +1,11 @@
 from langchain_mongodb import MongoDBAtlasVectorSearch
-from embeddingModel import embeddings
-from envVariables import MONGODB_COLLECTION, ATLAS_VECTOR_SEARCH_INDEX_NAME
+from embeddingModel import exported_embeddings
+from envVariables import MONGODB_COLLECTION, ATLAS_VECTOR_SEARCH_INDEX_NAME, MONGODB_CONNECTION_STRING
 
 vector_store = MongoDBAtlasVectorSearch(
-    embedding=embeddings,
+    embedding=exported_embeddings,
     collection=MONGODB_COLLECTION,
     index_name=ATLAS_VECTOR_SEARCH_INDEX_NAME,
     relevance_score_fn="cosine",
+    connection_string=MONGODB_CONNECTION_STRING
 )          
